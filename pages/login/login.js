6b9bf82b1b89f8769ff71cca6b0c95177c9a4598
Tay <iamtayleung@gmail.com>
2022-03-25 13:59:19 +1100

    feat: created login css, and pasted across some elements from figma

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import loginStyles from login.css
 
 export default function Home() {
   return (
     <main>
       <h1 className='red_text'>Home page</h1>
-      <h2 className={styles.rainbow_text}>Some rainbow text</h2>
+      <h2 className={loginStyles.title}>Sign up</h2>
     </main>
   );
 }
