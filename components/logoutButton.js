4216d9a5bd75c3c626992b5e8e595c067211bd0d
Joja81 <40449048+Joja81@users.noreply.github.com>
2022-03-25 16:00:02 +1100

    Updated logout button to interact with api

--- components/logoutButton.js
+++ components/logoutButton.js
@@ -2,9 +2,27 @@ import { useContext } from "react";
 import { UserContext } from "../lib/context";
 
 export default function LogoutButton() {
-  const { email, setEmail, setPassword } = useContext(UserContext);
+  const { email, setEmail, password, setPassword } = useContext(UserContext);
 
-  const resetLogin = (e) => {
+  const resetLogin = async (e) => {
+
+    try {
+      const response = await fetch(
+        "https://authentication-seng2021.herokuapp.com/logout",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: email,
+            password: password,
+          }),
+        }
+      );
+    } catch {}
+
+
     setEmail(null);
     setPassword(null);
   }
