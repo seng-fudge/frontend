6ed7592119c9aaa787b4a5fe0f2d7148c0781561
Tay <iamtayleung@gmail.com>
2022-03-27 17:45:25 +1100

    feat: changed creation api, post request reflects input

--- pages/sendInvoice.js
+++ pages/sendInvoice.js
@@ -41,29 +41,50 @@ function InvoiceCreationForm() {
           },
           body: JSON.stringify({
             // your expected POST request payload goes here
-            InvoiceTypeCode: 0,
-            AllowanceCharge: {
-                ChargeIndicator: formChargeIndicator,
-                AllowanceChargeReason: formAllowanceChargeReason,
-                Amount: 0,
-                TaxCategory: {
-                    ID: formTaxCategoryID,
-                    Percent: 0,
-                    TaxScheme: {
-                        ID: formTaxSchemeID,
-                    }
-                }
-            },
-            LegalMonetaryTotal: {
-                LineExtensionAmount: 0,
-                TaxExclusiveAmount: 0,
-                TaxInclusiveAmount: 0,
-                ChargedTotalAmount: 0,
-                PayableAmount: 0
-            },
-            InvoiceLine: [
-                {}
-            ]
+            UBLID: 2.1,
+            CustomizationID: "urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0",
+            ProfileID: "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0",
+            ID: "EBWASP1002",
+            IssueDate: formIssueDate,
+            InvoiceCode: 380,
+            Currency: "AUD",
+            BuyerReference: "EBWASP1002",
+            AddDocReference: "ebwasp1002",
+            SupplierID: 80647710156,
+            SupplierStreet: "100 Business Street",
+            SupplierCity: "Dulwich Hill",
+            SupplierPost: 2203,
+            SupplierCountry: "AU",
+            SupplierRegistration: "Ebusiness Software Services Pty Ltd",
+            CustomerStreet: "Suite 132 Level 45",
+            CustomerAddStreet: "999 The Crescent",
+            CustomerCity: "Homebush West",
+            CustomerPost: 2140,
+            CustomerCountry: "AU",
+            CustomerRegistration: "Awolako Enterprises Pty Ltd",
+            PaymentType: 1,
+            PaymentID: "EBWASP1002",
+            PaymentTerms: "As agreed",
+            TaxAmount: 10,
+            TaxableAmount: 100,
+            TaxSubtotalAmount: 10,
+            TaxID: S,
+            TaxPercent: 10,
+            TaxSchemeID: GST,
+            LegalLineExtension: 100,
+            TaxExclusiveAmount: 100,
+            TaxInclusiveAmount: 110,
+            PayableRoundingAmount: 0,
+            PayableAmount: 110,
+            InvoiceID: 1,
+            InvoiceQuantity: 500,
+            InvoiceLineExtension: 100,
+            InvoiceName: "Pencils",
+            InvoiceTaxID: 5,
+            InvoiceTaxPercent: 10,
+            InvoiceTaxSchemeID: GST,
+            InvoicePriceAmount: 0.2,
+            InvoiceBaseQuantity: 1
           }),
         }
       );
