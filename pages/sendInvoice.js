eaccaf0dc69fa7f8b46f23802d14eb950c5d2a24
Tay <iamtayleung@gmail.com>
2022-03-28 22:12:40 +1100

    feat: changed create API, changed form inputs

--- pages/sendInvoice.js
+++ pages/sendInvoice.js
@@ -33,7 +33,7 @@ function InvoiceCreationForm() {
 
     try {
       const response = await fetch(
-        "http://einvoicecupcakes.herokuapp.com/data/read/v1",
+        "https://seng-test.azurewebsites.net/json/convert",
         {
           method: "POST",
           headers: {
@@ -44,45 +44,53 @@ function InvoiceCreationForm() {
             UBLID: 2.1,
             CustomizationID: "urn:cen.eu:en16931:2017#conformant#urn:fdc:peppol.eu:2017:poacc:billing:international:aunz:3.0",
             ProfileID: "urn:fdc:peppol.eu:2017:poacc:billing:01:1.0",
+            
             ID: "EBWASP1002",
             IssueDate: formIssueDate,
             InvoiceCode: 380,
             Currency: "AUD",
             BuyerReference: "EBWASP1002",
             AddDocReference: "ebwasp1002",
+
             SupplierID: 80647710156,
             SupplierStreet: "100 Business Street",
             SupplierCity: "Dulwich Hill",
             SupplierPost: 2203,
             SupplierCountry: "AU",
             SupplierRegistration: "Ebusiness Software Services Pty Ltd",
+
             CustomerStreet: "Suite 132 Level 45",
             CustomerAddStreet: "999 The Crescent",
             CustomerCity: "Homebush West",
             CustomerPost: 2140,
             CustomerCountry: "AU",
             CustomerRegistration: "Awolako Enterprises Pty Ltd",
+            CustomerEmail: "email",
+            CustomerContactName: "Tay",
+
             PaymentType: 1,
             PaymentID: "EBWASP1002",
             PaymentTerms: "As agreed",
+
             TaxAmount: 10,
             TaxableAmount: 100,
             TaxSubtotalAmount: 10,
-            TaxID: S,
+            TaxID: "S",
             TaxPercent: 10,
-            TaxSchemeID: GST,
+            TaxSchemeID: "GST",
             LegalLineExtension: 100,
             TaxExclusiveAmount: 100,
             TaxInclusiveAmount: 110,
             PayableRoundingAmount: 0,
             PayableAmount: 110,
+
             InvoiceID: 1,
             InvoiceQuantity: 500,
             InvoiceLineExtension: 100,
             InvoiceName: "Pencils",
             InvoiceTaxID: 5,
             InvoiceTaxPercent: 10,
-            InvoiceTaxSchemeID: GST,
+            InvoiceTaxSchemeID: "GST",
             InvoicePriceAmount: 0.2,
             InvoiceBaseQuantity: 1
           }),
@@ -113,46 +121,211 @@ function InvoiceCreationForm() {
       <form onSubmit={() => onSubmit()}>
         <div>
           <input
-            name="chargeIndicator"
-            placeholder="Charge Indicator"
-            value={formChargeIndicator}
+            name="BuyerReference"
+            placeholder="Buyer reference"
+            value={formBuyerReference}
             className={styles.input}
           />
         </div>
 
-        <div>
-          <input
-            name="allowanceChargeReason"
-            placeholder="Allowance Charge Reason"
-            value={formAllowanceChargeReason}
-            className={styles.input}
-          />
+        <div className="split right">
+            <h2 className="title">Customer Details</h2>
+
+            <div>
+            <input
+                name="CustomerContactName"
+                placeholder="Name"
+                value={formCustomerContactName}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerRegistration"
+                placeholder="Registered Name"
+                value={formCustomerRegistration}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerEmail"
+                placeholder="Email"
+                value={formCustomerEmail}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerStreet"
+                placeholder="Street Address"
+                value={formCustomerStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerAddStreet"
+                placeholder="Additional Street Address"
+                value={formCustomerAddStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCity"
+                placeholder="City"
+                value={formCustomerCity}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerPost"
+                placeholder="Postcode"
+                value={formCustomerPost}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCountry"
+                placeholder="Country"
+                value={formCustomerCountry}
+                className={styles.input}
+            />
+            </div>
+            
         </div>
 
-        <div>
-          <input
-            name="taxCategoryID"
-            placeholder="Tax Category ID"
-            value={formTaxCategoryID}
-            className={styles.input}
-          />
+        <div className="split left">
+            <h2 className="title">Customer Details</h2>
+
+            <div>
+            <input
+                name="CustomerRegistration"
+                placeholder="Registered Name"
+                value={formCustomerRegistration}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerStreet"
+                placeholder="Street Address"
+                value={formCustomerStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerAddStreet"
+                placeholder="Additional Street Address"
+                value={formCustomerAddStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCity"
+                placeholder="City"
+                value={formCustomerCity}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerPost"
+                placeholder="Postcode"
+                value={formCustomerPost}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCountry"
+                placeholder="Country"
+                value={formCustomerCountry}
+                className={styles.input}
+            />
+            </div>
         </div>
 
-        <div>
-          <input
-            name="taxSchemeID"
-            placeholder="Tax Scheme ID"
-            value={formTaxSchemeID}
-            className={styles.input}
-          />
+        <div className="split right">
+            <h2 className="title">Invoice Details</h2>
+
+            <div>
+            <input
+                name="InvoiceQuantity"
+                placeholder="Invoice Quantity"
+                value={formInvoiceQuantity}
+                type="number"
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerStreet"
+                placeholder="Street Address"
+                value={formCustomerStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerAddStreet"
+                placeholder="Additional Street Address"
+                value={formCustomerAddStreet}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCity"
+                placeholder="City"
+                value={formCustomerCity}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerPost"
+                placeholder="Postcode"
+                value={formCustomerPost}
+                className={styles.input}
+            />
+            </div>
+
+            <div>
+            <input
+                name="CustomerCountry"
+                placeholder="Country"
+                value={formCustomerCountry}
+                className={styles.input}
+            />
+            </div>
         </div>
+        
 
         <button
           type="submit"
           className={"btn-gradient large"}
-        //   disabled={
-        //     !formEmail || !formPassword || !isValidEmail || !isValidPassword
-        //   }
         >
           Create Invoice
         </button>
