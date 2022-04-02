e13df7477760cb6b096fa553f89281f10eb7c888
Joshua Smee <smeejosh81@gmail.com>
2022-04-02 18:36:59 +1100

    Updated cookie issue

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { Toaster } from "react-hot-toast";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import NavBar from "../components/navbar";
 import { UserContext } from "../lib/context";
 import "../styles/globals.css";
@@ -7,10 +7,17 @@ import jsCookie from "js-cookie";
 
 function MyApp({ Component, pageProps }) {
 
-  const [createToken, setCreateToken] = useState(loadCookie("createToken"));
-  const [sendToken, setSendToken] = useState(loadCookie("sendToken"));
-  const [email, setEmail] = useState(loadCookie("email"));
-  const [token, setToken] = useState(loadCookie("token"));
+  const [createToken, setCreateToken] = useState(null);
+  const [sendToken, setSendToken] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setCreateToken(loadCookie("createToken"));
+    setSendToken(loadCookie("sendToken"));
+    setEmail(loadCookie("email"));
+    setToken(loadCookie("token"))
+  }, []);
 
   return (
     <UserContext.Provider
