df4c07191d8d79a390a2d98e77cca6fb04528d9e
Joshua Smee <smeejosh81@gmail.com>
2022-04-18 18:57:00 +1000

    Finished page

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,20 @@ import { UserContext } from "../lib/context";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  const { email, password, sendToken, createToken, customer } = useContext(UserContext);
+  const { email, password, sendToken, createToken, product } = useContext(UserContext);
+
+  const test = () => {
+    console.log(product);
+  }
 
   return email ? (
     <main className="full gradient">
         <Link href="/createInvoice/customerDetails" passHref>
           <button className="btn-outline-white centered extra-large">Create</button>
         </Link>
+        <button onClick={test}>
+          test
+        </button>
     </main>
   ) : (
     <Link href="/signup" passHref>
