100514f83ad3c3f329fde339d585fc013f19fa8a
Joshua Smee <smeejosh81@gmail.com>
2022-04-19 00:40:59 +1000

    Fixed progbar

--- components/Progbar.js
+++ components/Progbar.js
@@ -3,48 +3,47 @@ import { UserContext } from "../lib/context";
 import Link from "next/link";
 
 export default function ProgBar({ index }) {
-
   return (
     <div className="progbar">
-      <Link
-        href="/customer"
-        className={index == 0 ? "active" : ""}
-      >
-        Customer
+      <Link href="/customer" passHref>
+        <button className={index == 0 ? "active" : ""}>Customer</button>
       </Link>
-      <Link
-        href="/payment"
-        className={index == 1 ? "active" : ""}
-      >
-        Payment
+      <Link href="/payment" passHref>
+        <button className={index == 1 ? "active" : ""}>Payment</button>
       </Link>
-      <Link
-        href="/product"
-        className={index == 2 ? "active" : ""}
-      >
-        Product
+      <Link href="/product" passHref>
+        <button className={index == 2 ? "active" : ""}>Product</button>
       </Link>
-      <Link
-        href="/showInvoice"
-        className={index == 3 ? "active" : ""}
-      >
-        Preview
+      <Link href="/showInvoice" passHref>
+        <button className={index == 3 ? "active" : ""}>Preview</button>
       </Link>
 
       {index == 0 ? (
-        <Link href="/payment">❯</Link>
+        <Link href="/payment" passHref>
+          <button>❯</button>
+        </Link>
       ) : index == 1 ? (
         <>
-          <Link href="/customer">❮</Link>
-          <Link href="/product">❯</Link>
+          <Link href="/customer" passHref>
+            <button>❮</button>
+          </Link>
+          <Link href="/product" passHref>
+            <button>❯</button>
+          </Link>
         </>
       ) : index == 2 ? (
         <>
-          <Link href="/payment">❮</Link>
-          <Link href="/showInvoice">❯</Link>
+          <Link href="/payment" passHref>
+            <button>❮</button>
+          </Link>
+          <Link href="/showInvoice" passHref>
+            <button>❯</button>
+          </Link>
         </>
       ) : (
-        <Link href="/product">❮</Link>
+        <Link href="/product" passHref>
+          <button>❮</button>
+        </Link>
       )}
     </div>
   );
