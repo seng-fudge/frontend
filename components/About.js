7a90eb2ecec22ecc3eb851d059fc9282ba50111d
Joshua Smee <smeejosh81@gmail.com>
2022-05-11 16:22:59 +1000

    Add about to signup page

---
+++ components/About.js
@@ -0,0 +1,83 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function About() {
+  return (
+    <Parallax pages={3} style={{ top: "0", left: "0" }}>
+      <ParallaxLayer offset={0} speed={1} className="gradient" />
+
+      <ParallaxLayer
+        offset={0}
+        speed={2.5}
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginLeft: "50%",
+          marginRight: "0%",
+          width: "50%",
+        }}
+      >
+        <h1 className="white-title">FUDGE Invoice Creation</h1>
+      </ParallaxLayer>
+
+      <ParallaxLayer offset={0} sticky={{ start: 0, end: 3 }} style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          marginLeft: "0%",
+          marginRight: "50%",
+          width: "50%",
+        }}>
+        <Image src="/logo.png" objectFit="scale-down" layout="fill" alt="Logo"/>
+      </ParallaxLayer>
+
+      <ParallaxLayer
+        offset={1}
+        speed={1}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          color: "black",
+          marginLeft: "50%",
+          marginRight: "0%",
+          width: "40%",
+        }}
+      >
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?
+        </p>
+      </ParallaxLayer>
+      <ParallaxLayer
+        offset={2}
+        speed={3}
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginLeft: "50%",
+          marginRight: "0%",
+          width: "40%",
+        }}
+      >
+        <Link href="/signup" passHref>
+          <button className="btn-gradient large centered">Signup</button>
+        </Link>
+      </ParallaxLayer>
+    </Parallax>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { useContext } from "react";
+import About from "../components/About";
 import Loader from "../components/Loader";
 import { UserContext } from "../lib/context";
 import styles from "../styles/Home.module.css";
@@ -9,9 +10,6 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   const { email, password, sendToken, createToken, product } = useContext(UserContext);
 
-  const test = () => {
-    console.log(product);
-  }
 
   return email ? (
     <main className="full gradient">
@@ -27,8 +25,6 @@ export default function Home() {
         </div>
     </main>
   ) : (
-    <Link href="/signup" passHref>
-      <button className="btn-gradient large centered">Signup</button>
-    </Link>
+    <About/>
   );
 }
